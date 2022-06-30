14fef20c0f4ddb110f36f46ab8dfd46b8366476c
Marc-André Dumas <madumas@madumas.ca>
2022-06-30 13:49:48 -0400

    simpler fix

--- e2e/scripts/test.sh
+++ e2e/scripts/test.sh
@@ -6,9 +6,7 @@ start_rpcsplitter() {
     go mod download
 
     timeout=2
-    SMOCKER_IP=$(getent hosts smocker | cut -d " " -f1)
-    SMOCKER2=http://$SMOCKER_IP:8080
-    endpoints="$SMOCKER,$SMOCKER2,http://10.255.255.1"
+    endpoints="$SMOCKER,$SMOCKER/,http://10.255.255.1"
     (go run cmd/rpc-splitter/*.go run --log.format json --eth-rpc $endpoints -v debug -t $timeout)&
     sleep 3
     cd ..
